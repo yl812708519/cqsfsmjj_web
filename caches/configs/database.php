119482d1dd15383a114b34a6d926504d8c069758
杨璐 <812708519@qq.com>
2015-08-20 17:47:07 +0800

    init

---
+++ caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => '127.0.0.1',
+		'port' => 3306,
+		'database' => 'phpcms',
+		'username' => 'root',
+		'password' => '',
+		'tablepre' => '',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/install_package/',
+    'authkey' => 'ZToNkDUOW4G7696ePGT1G9ZXOzZWhHbe',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
